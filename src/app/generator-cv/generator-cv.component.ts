2441e388e14e1e139a116432faaece3519022bce
ach02raf <ach02raf@gmail.com>
2023-05-12 10:31:55 +0100

    5-12-2023 101h

--- src/app/generator-cv/generator-cv.component.ts
+++ src/app/generator-cv/generator-cv.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { CVService } from '../services/cv.service';
-
+import { jsPDF } from 'jspdf';
+import html2canvas from 'html2canvas';
 @Component({
   selector: 'app-generator-cv',
   templateUrl: './generator-cv.component.html',
@@ -10,6 +11,7 @@ import { CVService } from '../services/cv.service';
 })
 export class GeneratorCvComponent implements OnInit {
   constructor(private cvService: CVService) {}
+  @ViewChild('content', { static: false }) content: ElementRef;
   ngOnInit(): void {}
   active = 1;
   isCollapsed = true;
@@ -26,5 +28,18 @@ export class GeneratorCvComponent implements OnInit {
     this.isCollapsed = !this.isCollapsed;
 
     return this.hidden;
+  }
+  generatePdf() {
+    const doc = new jsPDF();
+    const htmlContent = this.content.nativeElement;
+
+    html2canvas(htmlContent).then((canvas) => {
+      const imgData = canvas.toDataURL('image/png');
+      const imgProps = doc.getImageProperties(imgData);
+      const pdfWidth = doc.internal.pageSize.getWidth();
+      const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+      doc.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+      doc.save('document.pdf');
+    });
   }
 }
